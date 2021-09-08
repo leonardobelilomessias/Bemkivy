963c1068c8d5d6b9efa174dcccfcd195756fb43f
leonardobmessias <71788533+leonardobmessias@users.noreply.github.com>
2021-09-08 18:07:13 -0300

    refatoração de codigo

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const app = express();
 
+app.set('view engine','ejs')
+app.use(express.static('public'))
+
 app.get("/",(req,res)=>{
-    res.render("index")    
+    res.render("index")
 })
+
+app.get("/login",(req,res)=>{
+    res.render("login")
+})
+
+app.get("/cadastro",(req,res)=>{
+    res.render("cadastro")
+})
+
+app.get("/user",(req,res)=>{
+    res.render("/users/index")
+})
+
 
 app.listen(8080,()=>{
     console.log('servidor funcionando')
-})
+});
